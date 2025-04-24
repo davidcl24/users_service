c8623621180a30040ec49dccff5d05e80ef6e17b
davidgit <davidcl.flor@gmail.com>
2025-04-24 21:01:02 +0200

    Added users router

---
+++ lib/users_service_web/controllers/user_json.ex
@@ -0,0 +1,27 @@
+defmodule UsersServiceWeb.UserJSON do
+  alias UsersService.Users.User
+
+  @doc """
+  Renders a list of users.
+  """
+  def index(%{users: users}) do
+    %{data: for(user <- users, do: data(user))}
+  end
+
+  @doc """
+  Renders a single user.
+  """
+  def show(%{user: user}) do
+    %{data: data(user)}
+  end
+
+  defp data(%User{} = user) do
+    %{
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      password_hash: user.password_hash,
+      signup_date: user.signup_date
+    }
+  end
+end
--- lib/users_service_web/router.ex
+++ lib/users_service_web/router.ex
@@ -7,6 +7,7 @@ defmodule UsersServiceWeb.Router do
 
   scope "/api", UsersServiceWeb do
     pipe_through :api
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
